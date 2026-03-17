c931e3df86337b6fab2d623605686a9cac497937
Alexander Andreev <wrkandreev@gmail.com>
2026-03-17 16:32:08 +0300

    wire homepage template through registry controller

--- core/custom/packages/Main/src/Controllers/BaseController.php
+++ core/custom/packages/Main/src/Controllers/BaseController.php
@@ -2,9 +2,37 @@
 
 namespace EvolutionCMS\Main\Controllers;
 
-class BaseController
+use EvolutionCMS\TemplateController;
+
+class BaseController extends TemplateController
 {
-    public function __construct()
+    protected array $data = [];
+    protected int $docid = 0;
+
+    public function process(): void
     {
+        $this->docid = (int) \evo()->documentIdentifier;
+
+        $this->data['pagetitle'] = $this->getDocumentField('pagetitle');
+        $this->data['introtext'] = $this->getDocumentField('introtext');
+
+        $this->setPageData();
+
+        $this->addViewData($this->data);
+    }
+
+    protected function setPageData(): void
+    {
+    }
+
+    protected function getDocumentField(string $key): string
+    {
+        $value = \evo()->documentObject[$key] ?? '';
+
+        if (is_array($value)) {
+            $value = $value[1] ?? $value[0] ?? '';
+        }
+
+        return trim((string) $value);
     }
 }
---
+++ core/custom/packages/Main/src/Controllers/HomeController.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace EvolutionCMS\Main\Controllers;
+
+class HomeController extends BaseController
+{
+}
